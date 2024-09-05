a6cc4f5352f9cf9008b2e7ed87fcf3983b8108bf
Rachel <hurui.lcy@gmail.com>
2024-09-05 14:51:36 -0600

    Math

---
+++ math-object/index.js
@@ -0,0 +1,14 @@
+//Math = build-in object that provides a collection of  properties and methods
+
+
+//console.log(Math.PI);
+
+let x = 3.21;
+let y = 2;
+//let q = 3.99;
+let z;
+
+//z = Math.round(x);
+z = Math.floor(q);
+//Math.round 是四舍五入； Math.floor是 rounds number down
+console.log(z);
