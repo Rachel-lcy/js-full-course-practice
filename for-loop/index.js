589d01aac9e62e2720ee4ece922889c076db74b2
Rachel <hurui.lcy@gmail.com>
2024-09-07 14:11:18 -0600

    for loop

---
+++ for-loop/index.js
@@ -0,0 +1,20 @@
+// for loop = repeat some code a LIMITED amount of times
+
+for (let i = 0; i <= 10; i++) {
+  //console.log("Hello");
+  console.log(i);
+}
+
+
+for (let i = 1; i <= 20; i++) {
+  if (i == 13) {
+    //continue;
+    //skip this iteration in this loop
+    break;
+  }
+  else {
+    console.log(i);
+  }
+
+
+}
