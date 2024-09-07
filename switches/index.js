5b138c432e2f813594e7c1901110561ab6f46f7e
Rachel <hurui.lcy@gmail.com>
2024-09-06 21:46:51 -0600

    Switches

---
+++ switches/index.js
@@ -0,0 +1,48 @@
+// Switch = can be an efficient replacement to many else if statements
+/*
+let day = "5";
+switch (day) {
+  case 1:
+    console.log("It is the Monday!");
+    break;
+  case 2:
+    console.log("It is the Tuesday!");
+    break;
+  case 3:
+    console.log("It is the Wednesday!");
+    break;
+  case 4:
+    console.log("It is the Thursday!");
+    break;
+  case 5:
+    console.log("It is the Friday!");
+    break;
+  case 6:
+    console.log("It is the Saturday!");
+    break;
+  case 7:
+    console.log("It is the Sunday!");
+    break;
+  default:
+    console.log(`${day} is not a day`)
+}
+*/
+let testScore = 85;
+let letterGrade;
+
+switch (true) {
+  case testScore >= 90:
+    letterGrade = "A";
+    break;
+  case testScore >= 80:
+    letterGrade = "B";
+    break;
+  case testScore >= 70:
+    letterGrade = "C";
+    break;
+  case testScore >= 60:
+    letterGrade = "D";
+    break;
+  default:
+    letterGrade = "F";
+}
