e5b4e3a5e6d50d166e9ea2fd9e668c61e2dbf193
Rachel <hurui.lcy@gmail.com>
2024-09-11 13:29:13 -0600

    Arrays

---
+++ array/html.js
@@ -0,0 +1,22 @@
+// array = a variable like structure that can hold more than 1 value
+
+let fruits = [
+  "apple",
+  "orange",
+  "banana",
+  "mango"
+];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+//fruits[1] = "coconut";
+
+//fruits.push("coconut");
+
+//let numOfFruits = fruits.length;
+
+//console.log(fruits[0]);
+//console.log(fruits[1]);
+//console.log(fruits[2]);
+
+//console.log(numOfFruits);
